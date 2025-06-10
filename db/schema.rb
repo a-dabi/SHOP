c8b99b8b675c4a1d52d4b35363f0ad723211ca6e
a-dabi <adrian.dabi@gmail.com>
2025-06-09 17:37:53 -0700

    db migration added color, 4.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_09_230141) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_10_001116) do
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
     t.string "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
   end
 end
